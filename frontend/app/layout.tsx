045beb93bc96073e06e1f16457f650a7e019640b
Josevan Oliveira <josivantarcio@gmail.com>
2025-05-14 22:04:19 -0300

    fix: corrigir tipagem no layout.tsx do frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: any;
 }) {
   return (
     <html lang="pt-BR">
